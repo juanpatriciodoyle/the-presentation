3e8fabac0da4d7885caf0a48408aff874fb84eb0
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-07-26 20:49:35 +0200

    add header

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  text-align: center;
+  margin-bottom: 3rem;
+`;
+
+export const Title = styled.h2`
+  font-size: 2.25rem;
+  font-weight: 800;
+  letter-spacing: -0.02em;
+`;
+
+export const Description = styled.p`
+  margin-top: 1rem;
+  font-size: 1.125rem;
+  color: #6b7280;
+  max-width: 48rem;
+  margin-left: auto;
+  margin-right: auto;
+`;
